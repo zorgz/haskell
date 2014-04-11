0cc6e3a2670e9ea601005e58635952878519d32f
Aleksey Shabanov <ashabanov@synapsense.com>
2014-04-11 12:43:20 -0700

    removed sublime workspace

--- amazon/Subarrays.hs
+++ amazon/Subarrays.hs
@@ -5,8 +5,9 @@ findSubarrays x = divideSorted (reverse $ sort x) 0 0 ([],[])
 
 divideSorted ::(Num a, Ord a) =>  [a] -> a -> a -> ([a], [a]) -> ([a],[a])
 divideSorted [] _ _ (f,s) = (f,s)
-divideSorted (x:y:xs) _ _ ([],[]) = divideSorted xs x y ([x], [y])
+divideSorted (x:y:xs) _ _ ([],[]) = divideSorted xs (abs x) (abs y) ([x], [y])
 divideSorted (x:xs) fcnt scnt (f,s) =
-	let firstIsLarger = abs (fcnt + x) > abs (scnt + x)
- 	in if firstIsLarger then divideSorted xs fcnt (scnt+x) (f, s ++ [x])
- 	   else divideSorted xs (fcnt+x) scnt (f ++ [x], s)
+	let absx = abs x
+	    firstIsLarger = (fcnt + absx) > abs (scnt + absx)
+ 	in if firstIsLarger then divideSorted xs fcnt (scnt+absx) (f, s ++ [x])
+ 	   else divideSorted xs (fcnt+absx) scnt (f ++ [x], s)
