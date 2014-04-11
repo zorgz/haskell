53badbf0da4d980bad59a1694e6c438d06dbe371
Aleksey Shabanov <ashabanov@synapsense.com>
2014-04-11 10:45:15 -0700

    +abs

--- amazon/Subarrays.hs
+++ amazon/Subarrays.hs
@@ -1,12 +1,12 @@
-import Data.List (sort)  
--- 
+import Data.List (sort)
+--
 findSubarrays ::(Num a, Ord a) =>  [a] -> ([a],[a])
 findSubarrays x = divideSorted (reverse $ sort x) 0 0 ([],[])
 
 divideSorted ::(Num a, Ord a) =>  [a] -> a -> a -> ([a], [a]) -> ([a],[a])
 divideSorted [] _ _ (f,s) = (f,s)
 divideSorted (x:y:xs) _ _ ([],[]) = divideSorted xs x y ([x], [y])
-divideSorted (x:xs) fcnt scnt (f,s) = 
-	let firstIsLarger = (fcnt + x) > (scnt + x)
- 	in if firstIsLarger then divideSorted xs fcnt (scnt+x) (f, s ++ [x]) 
+divideSorted (x:xs) fcnt scnt (f,s) =
+	let firstIsLarger = abs (fcnt + x) > abs (scnt + x)
+ 	in if firstIsLarger then divideSorted xs fcnt (scnt+x) (f, s ++ [x])
  	   else divideSorted xs (fcnt+x) scnt (f ++ [x], s)
